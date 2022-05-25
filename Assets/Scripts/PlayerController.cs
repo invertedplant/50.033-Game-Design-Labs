89bd380f1900dcaab13407969fc04e5ed1dc198b
invertedplant <invertedplant@gmail.com>
2022-05-26 01:49:21 +0800

    First Commit
    Files everywhere!

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,107 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using System;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 70;
+    public float upSpeed = 20;
+    public float maxSpeed = 10;
+    public Transform enemyLocation;
+    public TMP_Text scoreText;
+    private int score = 0;
+    private bool countScoreState = false;
+    private Rigidbody2D marioBody;
+    private bool onGroundState = true;
+    private SpriteRenderer marioSprite;
+    private bool faceRightState = true;
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.CompareTag("Ground"))
+        {
+            onGroundState = true; // back on ground
+            countScoreState = false; // reset score state
+            scoreText.text = "Score: " + score.ToString();
+        };
+    }
+
+    // Even before you start, can be called many times
+    void Awake()
+    {
+
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Set to be 30 FPS
+        Application.targetFrameRate = 30;
+        marioBody = GetComponent<Rigidbody2D>();
+        marioSprite = GetComponent<SpriteRenderer>();
+    }
+
+    void FixedUpdate()
+    {
+        if (Input.GetKeyUp("a") || Input.GetKeyUp("d"))
+        {
+            marioBody.velocity = Vector2.zero;
+        }
+
+        if (Input.GetKeyDown("space") && onGroundState)
+        {
+            marioBody.AddForce(Vector2.up * upSpeed, ForceMode2D.Impulse);
+            onGroundState = false;
+            countScoreState = true; //check if Gomba is underneath
+        }
+
+        // dynamic rigidbody
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        if (Mathf.Abs(moveHorizontal) > 0)
+        {
+            Vector2 movement = new Vector2(moveHorizontal, 0);
+            if (marioBody.velocity.magnitude < maxSpeed)
+                marioBody.AddForce(movement * speed);
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // toggle state
+        if (Input.GetKeyDown("a") && faceRightState)
+        {
+            faceRightState = false;
+            marioSprite.flipX = true;
+        }
+
+        if (Input.GetKeyDown("d") && !faceRightState)
+        {
+            faceRightState = true;
+            marioSprite.flipX = false;
+        }
+        // when jumping, and Gomba is near Mario and we haven't registered our score
+        if (!onGroundState && countScoreState)
+        {
+            if (Mathf.Abs(transform.position.x - enemyLocation.position.x) < 0.5f)
+            {
+                countScoreState = false;
+                score++;
+                Debug.Log(score);
+            }
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Enemy"))
+        {
+            Debug.Log("Collided with Gomba!");
+            marioBody.velocity = Vector2.zero;
+            // this.enabled = false;
+            Time.timeScale = 0.0f;
+        }
+    }
+}
